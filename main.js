41eb10ad6efe56f8917d4c6b71de1d0d1fe028d7
RaymondC181 <99200228+RaymondC181@users.noreply.github.com>
2022-11-19 21:50:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,37 @@ function fn1()
     {
         console.log("You are correct");
     }
+    else if(str > (x-5) && str < (x+5))              //(x > low && x < high)
+    {
+        console.log("Very Hot");
+    }
+    else if(str > (x-8) && str < (x+8))              
+    {
+        console.log("Hot");
+    }
+    else if(str > (x-15) && str < (x+15))             
+    {
+        console.log("Very Warm");
+    }
+    else if(str > (x-20) && str < (x+20))             
+    {
+        console.log("Warm");
+    }
+    else if(str > (x-30) && str < (x+30))             
+    {
+        console.log("Cool");
+    }
+    else if(str > (x-40) && str < (x+40))             
+    {
+        console.log("Very Cool");
+    }
+    else if(str > (x-55) && str < (x+55))             
+    {
+        console.log("Cold");
+    }
+    else{
+        console.log("Very Cold");
+    }
 }
 
 function initialize()
