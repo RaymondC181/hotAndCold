8dbc611fdf026e8c57e438a12fa487c31e5a7eed
RaymondC181 <99200228+RaymondC181@users.noreply.github.com>
2022-11-19 20:32:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ function fn1()
 {
     var str = document.getElementById("text1").value; 
     console.log("Value inside text box is: " + str);
+    if(str==x)
+    {
+        console.log("You are correct");
+    }
 }
 
 function initialize()
